ec0f0a4b67fc8fa780bcd16d943a0b643c7ccb87
esatrait <elena.satraitis@gmail.com>
2023-01-09 23:02:08 -0600

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,26 @@
 import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
+import NextQuestion from "./NextQuestion";
+import Question from "./Question";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  let currentQuestion = 0;
+  let totalQuestions = data.length;
+
+  return (
+    <div className="app">
+      <h1>Trivia!</h1>
+      <h2>
+        Question {currentQuestion} of {totalQuestions}
+      </h2>
+      <Question
+        text={data[currentQuestion].question.text}
+        choices={data[currentQuestion].question.choices}
+      />
+      <NextQuestion />
+    </div>
+  );
 }
 
 export default App;
